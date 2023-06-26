b6d579d20ea42d8d2a2a9a515206040a586e8655
Chaochao <zhangchch4614@sina.cn>
2023-06-26 23:02:44 +0800

    JWT token生成

--- onlineselfservice/src/main/java/com/bmwfs/MainWebApplication.java
+++ onlineselfservice/src/main/java/com/bmwfs/MainWebApplication.java
@@ -1,5 +1,6 @@
 package com.bmwfs;
 
+import com.bmwfs.utils.TokenGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
---
+++ onlineselfservice/src/main/java/com/bmwfs/utils/TokenGenerator.java
@@ -0,0 +1,51 @@
+package com.bmwfs.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.Date;
+
+public class TokenGenerator {
+    private static final String SECRET_KEY = "e6MSMcPtHNp7pz4dXiBGW+/vX+N3bEsNJIzR8r1KHBc=";
+    private static final long EXPIRATION_TIME = 3600000;
+
+    public static String generateToken(String userId) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
+
+        JwtBuilder jwtBuilder = Jwts.builder()
+                .setSubject(userId)
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY);
+
+        jwtBuilder.claim("role", "admin");
+        String token = jwtBuilder.compact();
+        return token;
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static String getSubjectFromToken(String token) {
+        Claims claims = Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
+        return claims.getSubject();
+    }
+
+    public static String generateSecretKey() {
+        byte[] secretBytes = new byte[32];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(secretBytes);
+        return Base64.getEncoder().encodeToString(secretBytes);
+    }
+}
