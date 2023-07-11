d6a7318788409ff7577a3b6d298710eccb5d37bb
Chaochao <zhangchch4614@sina.cn>
2023-07-12 07:37:08 +0800

    promise，和 computed

---
+++ ggs/src/components/Demo.vue
@@ -0,0 +1,25 @@
+<template>
+    <input v-model="computedValue">
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      value: {
+        type: String,
+        default: ''
+      }
+    },
+    computed: {
+      computedValue: {
+        get() {
+          return this.value.toUpperCase();
+        },
+        set(newValue) {
+          const lowerCaseValue = newValue.toLowerCase();
+          this.$emit('update:value', lowerCaseValue);
+        }
+      }
+    }
+  }
+  </script>
