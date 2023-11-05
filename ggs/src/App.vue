471db6a2a0e5bc7a15d27554427c6b31e19bd5f4
Chaochao <zhangchch4614@sina.cn>
2023-11-05 11:20:34 +0800

    test

--- ggs/src/App.vue
+++ ggs/src/App.vue
@@ -1,25 +1,258 @@
 <template>
-    <div class="page">
-        <p v-test>hello world</p>
-        <h1 print-content  v-lang>Click me to print content</h1>
+    <div class="page1">
+        <div class="page">
+            <!-- <div class="header">
+            header
+        </div> -->
+            <div class="main" v-dragZoom id="draggable-element">
+                main<br />
+                main<br />
+                main<br />
+                main<br />
+                main<br />
+                main<br />
+                main<br />
+
+            </div>
+            <div class="footer">
+                footer
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import Hammer from "hammerjs";
 
 export default {
     data() {
         return {};
     },
-    created() {
+    mounted() {
+        //  this.move();
+        // this.test2();
+    },
+    methods: {
+        test() {
+            var getCss = function (o, key) {
+                return o.currentStyle
+                    ? o.currentStyle[key]
+                    : document.defaultView.getComputedStyle(o, false)[key];
+            };
 
-    }
+            const element = document.getElementById("draggable-element");
+            const hammer = new Hammer(element);
+            element.style.transform = "scale(1)"; // 初始缩放为1
+            element.style.top = "0";
+            element.style.left = "0";
+
+            let x = 0;
+            let y = 0;
+            let scale = 1;
+
+            hammer.get("pan").set({ direction: Hammer.DIRECTION_ALL }); // 设置拖拽方向为任意
+
+            hammer.on("panstart", function (event) {
+                x = parseInt(element.style.left);
+                y = parseInt(element.style.top);
+            });
+
+            hammer.on("panmove", function (event) {
+                var m = Math.max(y + event.deltaY, 0);
+                var n = Math.max(x + event.deltaX, 0);
+
+                //             const containerRect = element.getBoundingClientRect();
+                //   const targetRect = target.getBoundingClientRect();
+                //   const scaledWidth = parseFloat(getComputedStyle(target).width);
+
+                //   const x = targetRect.left - containerRect.left;
+                //   const y = targetRect.top - containerRect.top;
+                //   console.log(`Left: ${x}, Top: ${y}`);
+
+                element.style.top = m + "px";
+                element.style.left = n + "px";
+
+                // event?.preventDefault(); // 阻止默认行为
+                // event?.stopPropagation(); // 阻止事件冒泡
+            });
+
+            // hammer.on("panend", function (event) {});
+
+            // hammer.get("pinch").set({ enable: true });
+
+            // hammer.on("pinchmove", function (event) {
+            //     const distance = event.distance; // 两个手指之间的距离
+            //     const scale = distance / 100; // 自定义缩放比例系数
+
+            //     element.style.transform = `scale(${scale})`;
+            //     event.preventDefault();
+            // });
+
+            // hammer.on("pinchend", function () {
+            //     hammer.get("pinch").set({ enable: true });
+            // });
+        },
+        test2() {
+            let initialDistance;
+            let initialWidth;
+
+            const target = document.getElementById("draggable-element");
+
+            // 添加触摸事件监听器
+            target.addEventListener("touchstart", (e) => {
+                if (e.touches.length === 2) {
+                    // 计算两个触摸点的初始距离
+                    initialDistance = Math.hypot(
+                        e.touches[0].clientX - e.touches[1].clientX,
+                        e.touches[0].clientY - e.touches[1].clientY
+                    );
+                    initialWidth = parseFloat(getComputedStyle(target).width);
+                }
+            });
+
+            target.addEventListener("touchmove", (e) => {
+                if (e.touches.length === 2) {
+                    // 计算当前两个触摸点的距离
+                    const currentDistance = Math.hypot(
+                        e.touches[0].clientX - e.touches[1].clientX,
+                        e.touches[0].clientY - e.touches[1].clientY
+                    );
+
+                    // 计算缩放比例
+                    const scale = currentDistance / initialDistance;
+
+                    // 根据缩放比例调整目标元素的大小
+                    // target.style.width = initialWidth * scale + "px";
+                    target.style.transform = `scale(${scale})`;
+                }
+            });
+
+            // 添加鼠标事件监听器（用于在桌面上模拟手势）
+            target.addEventListener("mousedown", (e) => {
+                if (e.buttons === 1) {
+                    initialDistance = Math.hypot(0, 0);
+                    initialWidth = parseFloat(getComputedStyle(target).width);
+                }
+            });
+
+            target.addEventListener("mousemove", (e) => {
+                if (e.buttons === 1) {
+                    const currentDistance = Math.hypot(
+                        e.movementX,
+                        e.movementY
+                    );
+                    const scale = currentDistance / initialDistance;
+                    target.style.width = initialWidth * scale + "px";
+                }
+            });
+        },
+        test3() {
+            let initialDistance;
+            let initialScale = 1;
+            const container = document.getElementById("draggable-element");
+            const target = document.getElementById("draggable-element");
+
+            // 添加触摸事件监听器
+            container.addEventListener("touchstart", (e) => {
+                if (e.touches.length === 2) {
+                    // 计算两个触摸点的初始距离
+                    initialDistance = Math.hypot(
+                        e.touches[0].clientX - e.touches[1].clientX,
+                        e.touches[0].clientY - e.touches[1].clientY
+                    );
+                    initialScale = parseFloat(
+                        getComputedStyle(target).transform.match(
+                            /-?\d*(\.\d+)?/
+                        )[0]
+                    );
+                }
+            });
+
+            container.addEventListener("touchmove", (e) => {
+                if (e.touches.length === 2) {
+                    // 计算当前两个触摸点的距离
+                    const currentDistance = Math.hypot(
+                        e.touches[0].clientX - e.touches[1].clientX,
+                        e.touches[0].clientY - e.touches[1].clientY
+                    );
+
+                    // 计算缩放比例
+                    const scale =
+                        (currentDistance / initialDistance) * initialScale;
+
+                    // 对目标元素进行缩放
+                    target.style.transform = `scale(${scale})`;
+                }
+            });
+        },
+
+        move() {
+            const target = document.getElementById("draggable-element");
+            let isDragging = false;
+            let offsetX, offsetY;
+
+            target.addEventListener("touchstart", (e) => {
+              
+                const touch = e.touches[0];
+                // console.log(touch,touch.clientX ,touch.clientY);
+                offsetX = touch.clientX - target.getBoundingClientRect().left;
+                offsetY = touch.clientY - target.getBoundingClientRect().top;
+                isDragging = true;
+            });
+
+            target.addEventListener("touchmove", (e) => {
+                if (isDragging) {
+                    const touch = e.touches[0];
+                    const newX = touch.clientX - offsetX;
+                    const newY = touch.clientY - offsetY;
+                    target.style.left = `${newX}px`;
+                    target.style.top = `${newY}px`;
+                }
+            });
+
+            target.addEventListener("touchend", () => {
+                isDragging = false;
+            });
+        },
+    },
 };
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style lang="scss">
 * {
     box-sizing: border-box;
+}
+
+.page1 {
+    position: fixed;
+    top: 20px;
+    bottom: 20px;
+    left: 0;
+    right: 0;
+    border: 1px solid red;
 }
 
 .page {
@@ -28,20 +261,46 @@ export default {
     bottom: 0;
     left: 0;
     right: 0;
-    border: 1px solid red;
+
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    // min-height: 100%;
+    // height: 100%;
+    // min-height: 0;
+
+    overflow: auto;
+
+    &::before {
+        content: "";
+        display: block;
+        width: 100%;
+        min-height: 3rem;
+        border: 1px solid blue;
+    }
 }
 
+.header {
+    height: 3rem;
+    border: 1px solid blue;
+
+    // flex-basis: auto;
+    flex: 0 0 auto; /* 固定高度 */
+}
 .footer {
-    position: absolute; /* 将.footer元素设置为绝对定位，以便将其置于底部 */
-    bottom: -120px; /* 将.footer元素放置在.asb容器的底部 */
-    width: 100%; /* 设置.footer元素的宽度占满整个容器 */
-    background-color: red; /* 设置.footer的背景颜色，可根据需要修改 */
-    padding: 10px; /* 设置.footer的内边距，可根据需要修改 */
-    height: 100px;
+    height: 5rem;
+    border: 1px solid blue;
+    flex: 0 0 auto; /* 固定高度 */
 }
 
-
-*{
-    user-select: none;
+.main {
+    will-change: transform;
+    position: absolute;
+    transform-origin: center center;
+    transform: scale(1);
+    // flex: 1;
+    background-color: yellow;
+    height: 300px;
+    width: 300px;
+    box-shadow: 1px 5px 10px gray;
 }
 </style>
--- ggs/src/main.js
+++ ggs/src/main.js
@@ -1,29 +1,31 @@
 import { ref, createApp } from 'vue'
 import App from './App.vue'
-import './core/loadAppsetting'
+
 // import router from './router'
 
 var app = createApp(App);
 const language = ref('zh');
 
 
+new (require('vconsole'))();
+
 app.directive('lang', {
     // 绑定到元素时调用
     bind(el, binding, vnode) {
         // 自定义指令的逻辑
         el.textContent = 'This is a custom directive for elements with lang="en".';
-      },
-  });
-  
-  app.directive('test', {
+    },
+});
+
+app.directive('test', {
     // 当指令绑定到元素时调用
     mounted(el, binding) {
-      // 添加点击事件处理程序
-      el.addEventListener('click', (e) => {
-        console.log('Button clicked!',el.textContent);
-      });
+        // 添加点击事件处理程序
+        el.addEventListener('click', (e) => {
+            console.log('Button clicked!', el.textContent);
+        });
     }
-  });
+});
 
 
 app.directive('localize', {
@@ -116,4 +118,161 @@ app.directive('copy', {
         });
     }
 });
+
+
+
+
+
+
+app.directive('drag-zoom', {
+    beforeMount(el, binding) {
+        let startX = 0;
+        let startY = 0;
+        let initialScale = 1;
+
+        el.addEventListener('touchstart', (e) => {
+            startX = e.touches[0].clientX;
+            startY = e.touches[0].clientY;
+
+            // initialScale = el.style.transform
+            //     ? parseFloat(el.style.transform.replace(/[^0-9.-]/g, ''))
+            //     : 1;
+        });
+
+        el.addEventListener('touchmove', (e) => {
+            const deltaX = e.touches[0].clientX - startX;
+            const deltaY = e.touches[0].clientY - startY;
+
+            el.style.left = el.offsetLeft + deltaX + 'px';
+            el.style.top = el.offsetTop + deltaY + 'px';
+
+            // const scale = e.touches.length === 2
+            //     ? (Math.hypot(
+            //         e.touches[0].clientX - e.touches[1].clientX,
+            //         e.touches[0].clientY - e.touches[1].clientY
+            //     ) / initialScale)
+            //     : 1;
+
+            // el.style.transform = `scale(${scale * initialScale})`;
+        });
+
+
+        // el.addEventListener('touchstart',(e)=>{
+
+        // })
+    }
+});
+
+
+
+
+
+app.directive('dragZoom', {
+    mounted(el, binding) {
+        let initialDistance;
+        let initialWidth;
+        let _scale;
+
+
+
+        let parentNode = el.parentNode;
+        let p_width = getComputedStyle(el.parentNode).width;
+        let p_height = getComputedStyle(el.parentNode).height;
+        let s_width = getComputedStyle(el).width
+        let s_height = getComputedStyle(el).height
+
+        s_width = 300;
+        s_height = 300;
+
+
+        let min_top = 0;
+        let min_left = 0;
+        let max_top = 0;
+        let max_left = 0;
+
+        el.addEventListener("touchstart", (e) => {
+            if (e.touches.length === 2) {
+                // 计算两个触摸点的初始距离
+                initialDistance = Math.hypot(
+                    e.touches[0].clientX - e.touches[1].clientX,
+                    e.touches[0].clientY - e.touches[1].clientY
+                );
+                initialWidth = parseFloat(getComputedStyle(el).width);
+            }
+
+            e?.preventDefault();
+        });
+
+        el.addEventListener("touchmove", (e) => {
+            if (e.touches.length === 2) {
+                // 计算当前两个触摸点的距离
+                const currentDistance = Math.hypot(
+                    e.touches[0].clientX - e.touches[1].clientX,
+                    e.touches[0].clientY - e.touches[1].clientY
+                );
+                const scale = currentDistance / initialDistance;
+                el.style.transform = `scale(${scale})`;
+            }
+
+            e?.preventDefault();
+        });
+
+
+        let isDragging = false;
+        let initialX, initialY;
+        const event = new Event('pan');
+
+        el.addEventListener('touchstart', (e) => {
+            const touch = e.touches[0];
+            initialX = touch.clientX - parseFloat(getComputedStyle(el).left);
+            initialY = touch.clientY - parseFloat(getComputedStyle(el).top);
+            isDragging = true;
+            e.preventDefault();
+        });
+
+        el.addEventListener('touchmove', (e) => {
+            if (isDragging) {
+                const touch = e.touches[0];
+
+                let top = touch.clientX - initialX;
+                let left = touch.clientY - initialY;
+                // let scale = new DOMMatrix(getComputedStyle(el).transform).a;
+                let scale = _scale ?? 1;
+
+                let offsetX = ((scale - 1) * s_width) / 2;
+                let offsetY = ((scale - 1) * s_height) / 2;
+
+                let newX = touch.clientX - initialX;
+                let newY = touch.clientY - initialY;
+
+                newX = newX - offsetX;
+                newY = newY - offsetY;
+
+                el.style.left = `${newX}px`;
+                el.style.top = `${newY}px`;
+
+
+
+            }
+            e.preventDefault();
+        });
+
+        el.addEventListener('touchend', (e) => {
+            isDragging = false;
+            e.preventDefault();
+        });
+
+
+    }
+})
+
+
+
+
+
+
+
+
+
+
 app.mount('#app')
