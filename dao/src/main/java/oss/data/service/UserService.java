fcd72339703723369b9b3dc39b6f266ee7300282
Chaochao <zhangchch4614@sina.cn>
2024-07-05 21:38:09 +0800

    test

--- dao/src/main/java/oss/data/service/UserService.java
+++ dao/src/main/java/oss/data/service/UserService.java
@@ -1,7 +1,17 @@
 package oss.data.service;
 
+import org.springframework.stereotype.Service;
+import oss.data.mapper.UserMapper;
+import oss.data.model.User;
+
+@Service
 
 public class UserService {
+    private UserMapper userMapper;
 
+
+    public User getUserById(int id){
+        return  userMapper.selectUser(id);
+    }
 
 }
